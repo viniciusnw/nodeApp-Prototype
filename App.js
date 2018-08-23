0d1285c7960fa74c46a8ba62ce82aab101a9c2bc
vinicius <vinicius.inacio@cmtecnologia.com.br>
2018-08-23 18:21:04 -0300

    add links for learn

---
+++ App.js
@@ -0,0 +1,12 @@
+https://scotch.io/tutorials/getting-to-know-flux-the-react-js-architecture
+https://www.npmjs.com/package/flux
+https://imasters.com.br/desenvolvimento/gerenciando-o-fluxo-assincrono-de-operacoes-em-nodejs
+
+https://docs.aws.amazon.com/pt_br/elasticbeanstalk/latest/dg/create_deploy_nodejs.html
+https://docs.aws.amazon.com/pt_br/elasticbeanstalk/latest/dg/nodejs-getstarted.html
+https://blog.risingstack.com/10-best-practices-for-writing-node-js-rest-apis/
+https://medium.com/codebase/structure-of-a-nodejs-api-project-cdecb46ef3f8
+
+https://github.com/gothinkster/node-express-realworld-example-app
+https://github.com/elsewhencode/project-guidelines#6-structure-and-naming
+
